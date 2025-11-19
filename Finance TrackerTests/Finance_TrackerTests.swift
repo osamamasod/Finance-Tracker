05f18f088c452f690da83550cdf324139ba4f7d4
Osama Masoud <osamamasoud07@gmail.com>
2025-11-19 18:07:41 +0700

    Initial Commit

---
+++ Finance TrackerTests/Finance_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_TrackerTests.swift
+//  Finance TrackerTests
+//
+//  Created by Osama Masoud on 19.11.2025.
+//
+
+import Testing
+@testable import Finance_Tracker
+
+struct Finance_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
